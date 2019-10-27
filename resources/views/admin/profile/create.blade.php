42d1216109b15113373d24fc16e3588d5c2ae804
fujiyoshi <oooo.yuriko@gmail.com>
2019-10-27 06:31:54 +0000

    php/laravel11

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,15 +1,16 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        
-    <meta charset="utf-8">
-    <meta http-epuiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width.initial-scale=1">
-    
-    <title>MyNews</title>
-</head>
+{{-- layouts/profile.blade.phpを読み込む --}}
+@extends('layouts.profile')
 
-<body>
-    <h1>こんにちは。</h1>
-</body>
-</html>
+
+@section('title', 'プロフィール作成')
+
+{{-- profile.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>プロフィール作成</h2>
+            </div>
+        </div>
+    </div>
+@endsection
--- resources/views/layouts/profile.blade.php
+++ resources/views/layouts/profile.blade.php
@@ -21,10 +21,9 @@
         <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
 
         <!-- Styles -->
-        {{-- Laravel標準で用意されているCSSを読み込みます --}}
-        <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
-        {{-- この章の後半で作成するCSSを読み込みます --}}
-        <link href="{{ secure_asset('css/admin.css') }}" rel="stylesheet">
+        {{-- php/laravel11 課題3 --}}
+        <link href="{{ secure_asset('css/profile.css') }}" rel="stylesheet">
+       
     </head>
     <body>
         <div id="app">
