6dada601035560c6c6ea57e5599846ec4b30c250
fujiyoshi <oooo.yuriko@gmail.com>
2019-11-09 08:50:07 +0000

    php/laravel12

--- routes/web.php
+++ routes/web.php
@@ -46,7 +46,7 @@ Route::group(['prefix' => 'admin'], function() {
 
     Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth');
     
-    Route::get('profile/edit', 'Admin\ProfileController@add')->middleware('auth');
+    Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
     
     Route::post('news/create', 'Admin\NewsController@create');
     
